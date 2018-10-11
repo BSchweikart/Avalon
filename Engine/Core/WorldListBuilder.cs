e04bf341b8d2f9887af618e520c9f0472b0b3444
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2018-10-10 22:58:08 -0400

    Update WorldListBuilder.cs
    working on the list builder did edit the location for the last entry and moved the last to the end need to add in the options for rooms. Player, Level, Classes, and Race don't go here? as they are not needed for the list of the world?

--- Engine/Core/WorldListBuilder.cs
+++ Engine/Core/WorldListBuilder.cs
@@ -12,41 +12,93 @@ using System.IO;
  * This is the section that will use the StreamReader function and set the values.
  * 
  * 
- * 
+ * The list should start working and have a base for the others
  * 
  * 
  */
 
 namespace Engine
 {
-    public class WorldListBuilder
-    {
-        string[] weapons = File.ReadAllLines(@"../../../Core/Docs/Weapon.txt");
-        string[] classes = File.ReadAllLines(@"../../../Core/Docs/Classes.txt");
-        string[] monsters = File.ReadAllLines(@"../../../Core/Docs/Monster.txt");
-        string[] NPC = File.ReadAllLines(@"../../../Core/Docs/NPC.txt");
-        string[] player = File.ReadAllLines(@"../../../Core/Docs/Player.txt");
-        string[] potions = File.ReadAllLines(@"../../../Core/Docs/Potions.txt");
-        string[] races = File.ReadAllLines(@"../../../Core/Docs/Races.txt");
-        string[] rooms = File.ReadAllLines(@"../../../Core/Docs/Rooms.txt");
-        string[] treasure = File.ReadAllLines(@"../../../Core/Docs/Treasure.txt");
-        string[] levels = File.ReadAllLines(@"../../../Core/Docs/Level.txt");
+   public static class WorldListBuilder
+   { 
+      private static string[] ReadNextLine(StreamReader reader)
+      {
+          string[] line = reader.ReadLine().Split(' ');
+          return line;
+      }
+       
+      public static void Generate()
+      {
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Treasure.txt")) //start building 
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.Treasures.Add(new Treasure(name, disc));
+             }
+         }
+         
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Monsters.txt")) // Start building 
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.Monsters.Add(new Monster(name, disc));
+             }
+         }
+          
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Rooms.txt"))
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.Rooms.Add(new Room(name, disc));
+             }
+         }
+          
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Potions.txt"))
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.Potions.Add(new Potion(name, disc));
+             }
+         }
+          
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Weapons.txt"))
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.Weapons.Add(new Weapon(name, disc));
+             }
+         }
+          
+         using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/NPC.txt"))
+         {
+             while (!reader.EndOfStream)
+             {
+                 string name = reader.ReadLine();
+                 string disc = reader.Readline();
+                 WorldGenerator.NPCs.Add(new NPC(name, disc));
+             }
+         }
+           //string[] weapons = File.ReadAllLines(@"../../../Engine/Docs/Weapon.txt");
+        //string[] classes = File.ReadAllLines(@"../../../Engine/Docs/Classes.txt");
+        //string[] monsters = File.ReadAllLines(@"../../../Engine/Docs/Monster.txt");
+        //string[] NPC = File.ReadAllLines(@"../../../Engine/Docs/NPC.txt");
+        //string[] player = File.ReadAllLines(@"../../../Engine/Docs/Player.txt");
+        //string[] potions = File.ReadAllLines(@"../../../Engine/Docs/Potions.txt");
+        //string[] races = File.ReadAllLines(@"../../../Engine/Docs/Races.txt");
+        //string[] rooms = File.ReadAllLines(@"../../../Engine/Docs/Rooms.txt");
+        //string[] treasure = File.ReadAllLines(@"../../../Engine/Docs/Treasure.txt");
+        //string[] levels = File.ReadAllLines(@"../../../Engine/Docs/Level.txt");      
 
-        //test to build the treasure
-        //public static void Build()
-        //{
-        //    using (StreamReader reader = File.OpenText(@"../../../Engine/Docs/Treasure.txt"))
-        //    {
-        //        while (!reader.EndOfStream)
-        //        {
-        //            string name = reader.ReadLine();
-
-
-        //            // WorldGenerator.Treasure.Add(new Treasure(name));
-        //        }
-        //    }
-        //}
-
-
-    }
+      }
+   } 
 }
