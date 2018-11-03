2e987a6e5f36a422e90330ecb6eb4a1afe70ee6b
Jenica Miller <millerj6354@student.faytechcc.edu>
2018-11-03 18:55:32 -0400

    Working in look class

--- Engine/Commands/Look.cs
+++ Engine/Commands/Look.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Engine.Commands
 {
-    public class Look
+    public static class Look
     {
+        public static void Looking (string noun)
+            {
+
+            string CapNoun = CapWord.FirstCharToUpper(moun);
+            determineVerbType(CapNoun);
+
+            }
+
+        public static void Looking()
+            {
+
+            CurrentLocationClass.DisplayCurrentLocation();
+
+            }
+
+        public static void determineVerbType (string noun)
+            {
+            Item itemToLook = World.ItemByName(noun);
+            Weapon weaponToLook = World.WeaponByName(noun);
+            Monster monsterToLook = World.MonsterByName(noun);
+            NPC npcToLook = World.NPCByName(noun);
+
+            if (itemToLook != null)
+                {
+                foreach (InventoryItem item in Player.player.Inventory.ToList())
+                    {
+                    if (item.Details.Name == itemToLook.ToString())
+                        {
+                        LookAtItem(item);
+}
+
+}
+                foreach (Item rmItem in Player.CurrentLocation.RoomLoot.ToList())
+                    {
+                    if (rmItem.Name == itemToLook.Name)
+                        {
+                        LookAtItem(rmItem);
+}
+}
+}               
+}
     }
 }
