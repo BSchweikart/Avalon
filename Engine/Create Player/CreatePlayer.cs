eac2d803441ae1cbac5f720394cb80a9d1d2b313
Jenica Miller <millerj6354@student.faytechcc.edu>
2018-11-07 22:00:30 -0500

    Added CreatePlayer class.  Inputted some data.  Will finish up tomorrow.

---
+++ Engine/Create Player/CreatePlayer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Creat_Player
+{
+    public static class CreatePlayer
+    {
+        public static void CreatePlayerInstructions()
+        {
+            string name;
+            string className = "";
+            string raceName = "";
+            int aligment = 0;
+            int gold = 0;
+            int hp = 0;
+            bool validClass = false;
+            bool validRace = false;
+
+            Console.WriteLine("What is your name?");
+            Console.Write(">");
+            name = CapWord.FirstCharoUpper(Console.ReadLine());
+
+            while (validClass == false)
+            {
+                Console.WriteLine("Choose a class.");
+                Console.WriteLine("Warrior, Archer, Healer, Assissin, or Mage");
+                className = Console.ReadLine().ToLower();
+
+                if (className == "Warrior")
+                {
+                    gold = 100;
+                    validClass = true;
+                }
+                else if (className == "Archer")
+                {
+                    gold = 200;
+                    validClass = true;
+                }
+                else if (className == "Healer")
+                {
+                    gold = 300;
+                    validClass = true;
+                }
+                else if (className == "Assissin")
+                {
+                    gold = 400;
+                    validClass = true;
+                }
+                else if (className == "Mage")
+                {
+                    gold = 500;
+                    validClass = true;
+                }
+                else
+                {
+                    Console.WriteLine("Pick a valid class");
+                }
+            }
+
+            while (validRace == false)
+            {
+                Console.WriteLine("Choose a race.");
+                Console.WriteLine("Elf, Dwarf, Gaint, Fairy, Dragon");
+                className = Console.ReadLine().ToLower();
+
+                if (raceName == "Elf")
+                {
+                    validRace = true;
+                    hp = 100;
+                }
+                else if (raceName == "Dwarf")
+                {
+                    validRace = true;
+                    hp = 100;
+                }
+                else if (raceName == "Gaint")
+                {
+                    validRace = true;
+                    hp = 100;
+                }
+                else if (raceName == "Fairy")
+                {
+                    validRace = true;
+                    hp = 100;
+                }
+                else if (raceName == "Dragon")
+                {
+                    validRace = true;
+                    hp = 100;
+                }
+                else
+                {
+                    Console.WriteLine("Pick a valid race");
+                }
+            }
+        }
+    }
+}
