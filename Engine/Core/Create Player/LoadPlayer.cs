5935f26e4df6f1bd6e7ea7a2fdcc8d308ad02d1f
Jenica Miller <millerj6354@student.faytechcc.edu>
2018-12-06 21:21:31 -0500

    Fixed errors in load player and added data into worldgen

--- Engine/Core/Create Player/LoadPlayer.cs
+++ Engine/Core/Create Player/LoadPlayer.cs
@@ -8,82 +8,82 @@ using System.Xml;
 using System.Xml.Serialization;
 
 
-//namespace Engine
-//{
-//    class LoadPlayer
-//    {
-//    private static Weapon Equipt;
+namespace Engine
+{
+    class LoadPlayer
+    {
+        private static Weapon Equipt;
 
-//        //public static object World { get; private set; }
+        //public static object World { get; private set; }
 
-//        public static void LoadGame(string name)
-//        {
-//            if (!File.Exists(name + ".xml"))
-//            {
-//                Console.WriteLine("Player does not exist.");
-//                IsNewPlayer.NewPlayer();
-//            }
-//            else
-//            {
-//                Player._player = null;
-//                Player._player = CreatePlayerFromXmlString(File.ReadAllText(name + ".xml"));
-//            }
-//        }
+        public static void LoadGame(string name)
+        {
+            if (!File.Exists(name + ".xml"))
+            {
+                Console.WriteLine("Player does not exist.");
+                IsNewPlayer.NewPlayer();
+            }
+            else
+            {
+                Player._player = null;
+                Player._player = CreatePlayerFromXmlString(File.ReadAllText(name + ".xml"));
+            }
+        }
 
-//        public static Player CreatePlayerFromXmlString(string PLAYER_DATA_FILE_NAME)
-//    {
-//        try
-//        {
-//            XmlDocument playerData = new XmlDocument();
+        public static Player CreatePlayerFromXmlString(string PLAYER_DATA_FILE_NAME)
+        {
+            try
+            {
+                XmlDocument playerData = new XmlDocument();
 
-//            playerData.LoadXml(PLAYER_DATA_FILE_NAME);
-//            string playerName = playerData.SelectSingleNode("/Player/Stats/Name").InnerText;
-//            string classPlayer = playerData.SelectSingleNode("/Player/Stats/Class").InnerText;
-//            string racePlayer = playerData.SelectSingleNode("/Player/Stats/Race").InnerText;
-//            int gold = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Gold").InnerText);
-//            int xp = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/XP").InnerText);
-//            int level = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Level").InnerText);
-//            int hpCurrent = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/HpCurrent").InnerText);
-//            int hpMax = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/HpMax").InnerText);
-//            //int alignment = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Alignment").InnerText);
-//            int equiptString = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentWeapon").InnerText);//check later
-//            Player player = new Player(playerName, classPlayer, racePlayer, gold, xp, level, Equipt, hpCurrent, hpMax, false, true);
-//            int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentLocation").InnerText);
-//            Player.CurrentLocation = WorldGenerator.LocationID(currentLocationID);
+                playerData.LoadXml(PLAYER_DATA_FILE_NAME);
+                string playerName = playerData.SelectSingleNode("/Player/Stats/Name").InnerText;
+                string classPlayer = playerData.SelectSingleNode("/Player/Stats/Class").InnerText;
+                string racePlayer = playerData.SelectSingleNode("/Player/Stats/Race").InnerText;
+                int gold = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Gold").InnerText);
+                int xp = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/XP").InnerText);
+                int level = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Level").InnerText);
+                int hpCurrent = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/HpCurrent").InnerText);
+                int hpMax = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/HpMax").InnerText);
+                //int alignment = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Alignment").InnerText);
+                int equiptString = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentWeapon").InnerText);//check later
+                Player player = new Player(playerName, classPlayer, racePlayer, gold, xp, level, Equipt, hpCurrent, hpMax, false, true);
+                int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentLocation").InnerText);
+                Player.CurrentLocation = WorldGenerator.RoomByID(currentLocationID);
 
-//            if (playerData.SelectSingleNode("/Player/Stats/CurrentWeapon") != null)
-//            {
-//                int currentWeaponID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentWeapon").InnerText);
-//                 player.Equipt = WorldGenerator.WeaponID(currentWeaponID);
-//            }
+                if (playerData.SelectSingleNode("/Player/Stats/CurrentWeapon") != null)
+                {
+                    int currentWeaponID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentWeapon").InnerText);
+                    player.Equipt = WorldGenerator.WeaponByID(currentWeaponID);
+                }
 
-//            foreach (XmlNode node in playerData.SelectNodes("/Player/Inventory/Inventory"))
-//            {
-//                int id = Convert.ToInt32(node.Attributes["ID"].Value);
-//                int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
+                foreach (XmlNode node in playerData.SelectNodes("/Player/Inventory/Inventory"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
 
-//                if (id > 100 && id <= 200)
-//                {
-//                    for (int i = 0; i < quantity; i++)
-//                    {
-//                         player.AddItemToInventory(WorldGenerator.WeaponID(id));
-//                    }
-//                }
-//                else
-//                {
-//                    for (int i = 0; i < quantity; i++)
-//                    {
-//                         player.AddItemToInventory(WorldGenerator.ItemID(id));
-//                    }
-//                }
-//            }
-//             return player;
-//        }
-//        catch
-//        {
-//            //Console.WriteLine(ex.ToString());
-//            return null;
-//        }
-//    }
-//  }
-//}
+                    if (id > 100 && id <= 200)
+                    {
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            player.AddItemToInventory(WorldGenerator.WeaponByID(id));
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            player.AddItemToInventory(WorldGenerator.ItemByID(id));
+                        }
+                    }
+                }
+                return player;
+            }
+            catch
+            {
+                //Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
--- Engine/Core/WorldGenerator.cs
+++ Engine/Core/WorldGenerator.cs
@@ -28,10 +28,34 @@ namespace Engine
         public static readonly List<Potion> potions = new List<Potion>();
         public static readonly List<Weapon> weapons = new List<Weapon>();
         public static readonly List<NPC> npcs = new List<NPC>();
+        public static readonly List<Item> items = new List<Item>();
         public static List<Player> players = new List<Player>();
         public static List<Class> classes = new List<Class>();
         public static List<Race> races = new List<Race>();
 
-       
+        public static Room RoomByID(int id)
+        {
+            return Location.SingleOrDefault(x => x.ID == id);
+        }
+        //public static Room RoomByName(string name)
+        //{
+        //    return Location.SingleOrDefault(x => x.Name == name);
+        //}
+        public static Weapon WeaponByID(int id)
+        {
+            return weapons.SingleOrDefault(x => x.ID == id);
+        }
+        public static Weapon WeaponByName(string name)
+        {
+            return weapons.SingleOrDefault(x => x.Name == name);
+        }
+        public static Item ItemByID(int id)
+        {
+            return items.SingleOrDefault(x => x.ID == id);
+        }
+        public static Item ItemByName(string name)
+        {
+            return items.SingleOrDefault(x => x.Name == name);
+        }
     }
 }
