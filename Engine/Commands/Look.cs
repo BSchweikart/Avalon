47aaa8297418b17c688baebafc8f5178fe2ebebb
Jenica Miller <millerj6354@student.faytechcc.edu>
2018-12-08 20:09:49 -0500

    fixed look and added properties to inventory

--- Engine/Commands/Look.cs
+++ Engine/Commands/Look.cs
@@ -25,94 +25,93 @@ namespace Engine
             Monster monsterToLook = WorldGenerator.MonsterByName(noun);
             NPC NpcToLook = WorldGenerator.NPCByName(noun);
 
-            //if (itemToLook != null)
-            //{
-            //    foreach (Inventory item in Player._player.Inventory.ToList())
-            //    {
-            //        if (item.Details.Name == itemToLook.ToString())
-            //        {
-            //            LookAtItem(item);
-            //        }
-            //    }
+            if (itemToLook != null)
+            {
+                foreach (Inventory item in Player._player.Inventory.ToList())
+                {
+                    if (item.Details.Name == itemToLook.ToString())
+                    {
+                        LookAtItem(item);
+                    }
+                }
 
-            //    foreach (Item rmItem in Player.CurrentLocation.ItemRoom.ToList())
-            //    {
-            //        if (rmItem.Name == itemToLook.Name)
-            //        {
-            //            LookAtItem(rmItem);
-            //        }
-            //    }
-            //}
-        //    else if (weaponToLook != null)
-        //    {
-        //        foreach (Inventory weapon in Player._player.Inventory.ToList())
-        //        {
-        //            if (weapon.Details.Name == weaponToLook.Name)
-        //            {
-        //                LookAtWeapon(weapon);
-        //            }
-        //        }
-
-        //        foreach (Weapon rmWeapon in Player.CurrentLocation.ItemRoom.ToList())
-        //        {
-        //            if (rmWeapon.Name == weaponToLook.Name)
-        //            {
-        //                LookAtWeapon(rmWeapon);
-        //            }
-        //        }
-
-        //    }
-        //    else if (monsterToLook != null)
-        //    {
-        //        foreach (Monster mob in Player.CurrentLocation.MonsterRoom.ToList())
-        //        {
-        //            if (mob.Name == monsterToLook.Name)
-        //            {
-        //                LookAtMonster(mob);
-        //            }
-        //        }
-        //    }
-        //}
-        //    public static void LookAtItem(Inventory itemToLook)
-        //    {
-        //        Console.WriteLine("Item Name: " + itemToLook.Details);
-        //        Console.WriteLine("Description: " + itemToLook.Details);
-        //       // Console.WriteLine("Item Price: " + itemToLook.Price);
-        //    }
-        //    public static void LookAtItem(Item itemToLook)
-        //    {
-        //        Console.WriteLine("Item Name: " + itemToLook.Name);
-        //        //Console.WriteLine("Description: " + itemToLook.Desc);
-        //        Console.WriteLine("Item Price: " + itemToLook.Price);
+                foreach (Item rmItem in Player.CurrentLocation.ItemRoom.ToList())
+                {
+                    if (rmItem.Name == itemToLook.Name)
+                    {
+                        LookAtItem(rmItem);
+                    }
+                }
             }
-            //public static void LookAtWeapon(Inventory weaponToLook)
-            //{
-            //    Console.WriteLine("Weapon Name: " + weaponToLook.WeaponName);
-            //    Console.WriteLine("Description: " + weaponToLook.WeaponDesc);
-            //    Console.WriteLine("Damage: " + weaponToLook.WeaponDamage);
-            //    Console.WriteLine("Damage Type", weaponToLook.WeaponDamageType);
-            //    Console.WriteLine("Price: ", weaponToLook.WeaponPrice.ToString());
-            //}
-            //public static void LookAtWeapon(Weapon weaponToLook)
-            //{
-            //    Console.WriteLine("Weapon Name: " + weaponToLook.Name);
-            //    Console.WriteLine("Description: " + weaponToLook.Desc);
-            //    Console.WriteLine("Damage: " + weaponToLook.Damage);
-            //    Console.WriteLine("Damage Type", weaponToLook.DamageType);
-            //    Console.WriteLine("Price: ", weaponToLook.Price.ToString());
-            //}
-            //public static void LookAtMonster(Monster monsterToLook)
-            //{
-            //    Console.WriteLine("Name: " + monsterToLook.Name);
-            //    Console.WriteLine("Armor Class: " + monsterToLook.AC);
-            //    Console.WriteLine("Current Hitpoints: " + monsterToLook.hpCurrent);
-            //}
-            //public static void LookAtNpc(NPC npcToLook)
-            //{
-            //    Console.WriteLine("Name: " + npcToLook.NPCName);
-            //    Console.WriteLine("Class: " + npcToLook.NPCClass);
-            //    Console.WriteLine("Race: " + npcToLook.NPCRace);
-            // }
-}
-}
+            else if (weaponToLook != null)
+            {
+                foreach (Inventory weapon in Player._player.Inventory.ToList())
+                {
+                    if (weapon.Details.Name == weaponToLook.Name)
+                    {
+                        LookAtWeapon(weapon);
+                    }
+                }
 
+                foreach (Weapon rmWeapon in Player.CurrentLocation.ItemRoom.ToList())
+                {
+                    if (rmWeapon.Name == weaponToLook.Name)
+                    {
+                        LookAtWeapon(rmWeapon);
+                    }
+                }
+
+            }
+            else if (monsterToLook != null)
+            {
+                foreach (Monster mob in Player.CurrentLocation.MonsterRoom.ToList())
+                {
+                    if (mob.Name == monsterToLook.Name)
+                    {
+                        LookAtMonster(mob);
+                    }
+                }
+            }
+        }
+        public static void LookAtItem(Inventory itemToLook)
+        {
+            Console.WriteLine("Item Name: " + itemToLook.Details);
+            Console.WriteLine("Description: " + itemToLook.Details);
+            Console.WriteLine("Item Price: " + itemToLook.Price);
+        }
+        public static void LookAtItem(Item itemToLook)
+        {
+            Console.WriteLine("Item Name: " + itemToLook.Name);
+            Console.WriteLine("Description: " + itemToLook.Description);
+            Console.WriteLine("Item Price: " + itemToLook.Price);
+        }
+        public static void LookAtWeapon(Inventory weaponToLook)
+        {
+            Console.WriteLine("Weapon Name: " + weaponToLook.WeaponName);
+            Console.WriteLine("Description: " + weaponToLook.WeaponDesc);
+            Console.WriteLine("Damage: " + weaponToLook.WeaponDamage);
+            Console.WriteLine("Damage Type", weaponToLook.WeaponDamageType);
+            Console.WriteLine("Price: ", weaponToLook.WeaponPrice.ToString());
+        }
+        public static void LookAtWeapon(Weapon weaponToLook)
+        {
+            Console.WriteLine("Weapon Name: " + weaponToLook.Name);
+            Console.WriteLine("Description: " + weaponToLook.Description);
+            Console.WriteLine("Damage: " + weaponToLook.DamageMax);
+            Console.WriteLine("Damage Type", weaponToLook.DamageType);
+            Console.WriteLine("Price: ", weaponToLook.Price.ToString());
+        }
+        public static void LookAtMonster(Monster monsterToLook)
+        {
+            Console.WriteLine("Name: " + monsterToLook.Name);
+            //Console.WriteLine("Armor Class: " + monsterToLook.AC);
+            Console.WriteLine("Current Hitpoints: " + monsterToLook.HpCurrent);
+        }
+        //public static void LookAtNpc(NPC npcToLook)
+        //{
+        //    Console.WriteLine("Name: " + npcToLook.NPCName);
+        //    Console.WriteLine("Class: " + npcToLook.NPCClass);
+        //    Console.WriteLine("Race: " + npcToLook.NPCRace);
+        //}
+    }
+}
--- Engine/Core/Items/Inventory.cs
+++ Engine/Core/Items/Inventory.cs
@@ -11,18 +11,31 @@ namespace Engine
     {
         private Item _details;
         private int _quantity;
+        private string _wName;
+        private string _wDesc;
+        private string _wDamage;
+        private string _wDamageType;
+        private int _wPrice;
 
         public Item Details { get { return _details; } set { _details = value; OnPropertyChanged("Details"); } }
         public int Quantity { get {return _quantity; } set {_quantity = value; OnPropertyChanged("Quantity"); } }
+        public int ItemID { get { return Details.ID; } }
+        //public string Description { get { return Quantity > 1 ? Details.NamePlural : Details.Name; } }
+        public int Price { get { return Details.Price; } }
+        public string WeaponName { get { return _wName; } set { _wName = value; } }
+        public string WeaponDesc { get { return _wDesc; } set { _wDesc = value; } }
+        public string WeaponDamage { get { return _wDamage; } set { _wDamage = value; } }
+        public string WeaponDamageType { get { return _wDamageType; } set { _wDamageType = value; } }
+        public int WeaponPrice { get { return _wPrice; } set { _wPrice = value; } }
 
-        public string Description
+        public Inventory(Weapon weapon, int quantity)
         {
-            get { return Quantity > 1 ? Details.Name : Details.Description; }
-        }
-
-        public Inventory(Item details, int quantity)
-        {
-            Details = details;
+            WeaponName = weapon.Name;
+            WeaponDesc = weapon.Description;
+            WeaponDamage = weapon.DamageMax;
+            WeaponDamageType = weapon.DamageType;
+            WeaponPrice = weapon.Price;
+            Details = weapon;
             Quantity = quantity;
         }
 
